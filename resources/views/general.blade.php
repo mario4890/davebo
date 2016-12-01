ed3886a8b6a82e5a5268b3999cf2622a3c371c81
Mariusz <mariusz.bocz@positive-power.pl>
2016-12-01 21:08:55 +0100

    umieszczenie pliku na stronie

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -2,6 +2,30 @@
 
 @section('content')
 
-<div>lala</div>
+<div id="wrapper">
+    <img src="img/tekst.png" alt="" />
+    <nav class="icons">
+        <div class="right">
+            <a href="#" id="video_button_mute"><span class="icon sound">Wycisz</span></a>
+        </div>
+
+        <a href="#" id="video_button_quality"><span class="icon sd">SD</span></a>
+    </nav>
+</div>
+<div class="after">
+    <div class="contents">ALE ON GRA :P</div>
+</div>
+
+<div class="after middle">
+    <div class="contents">Zjedź w dół</div>
+</div>
+<div class="after secound">
+    <div class="contents">ALE ON GRA :P</div>
+</div>
+</div>
+<video id="bg" autoplay="autoplay" loop="loop">
+    <source src="{{ URL::asset('video/FunkinDance.webm') }}" type="video/webm" />
+    <source src="{{ URL::asset('video/FunkinDance.mp4') }}" type="video/mp4" />
+</video>
 
 @endsection
